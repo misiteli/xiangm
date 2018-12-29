8be9444a6f20fed3bfb608fee23a2c791fdc92b2
lisiyuan <1402740958@qq.com>
2018-12-29 16:13:46 +0800

    tui

---
+++ zouxiu/src/mysql/usergoods.sql
@@ -0,0 +1,2 @@
+INSERT INTO `usergoods` VALUES ('123', 4, '女士Ace条纹平跟鞋', 'images/weiyi.png', 'lee', '双子座', 5656, 1);
+INSERT INTO `usergoods` VALUES ('1234', 2, '女士Ace条纹平跟鞋', 'images/weiyi.png', 'lee', '澳门', 212, 100);
---
+++ zouxiu/src/mysql/yonghu.sql
@@ -0,0 +1,6 @@
+INSERT INTO `yonghu` VALUES (1, '123', '123');
+INSERT INTO `yonghu` VALUES (4, '1234', '123');
+INSERT INTO `yonghu` VALUES (6, '21313', '123');
+INSERT INTO `yonghu` VALUES (7, '10086', '12345');
+INSERT INTO `yonghu` VALUES (8, '10010', '123');
+INSERT INTO `yonghu` VALUES (9, 'li', '123');
